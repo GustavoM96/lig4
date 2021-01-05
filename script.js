cbae27bd5c212cede3a03ed5dd30d665a07dbf9e
Andre <simple_single@protonmail.com>
2021-01-05 14:58:31 -0400

    Criação da Função do Jogo

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //---Gustavo
 
 let b = 8
-
-
-
+let player=0;
+let count=5;
 
 //---Caio
 
@@ -14,18 +13,70 @@ let b = 8
 
 //---Jardel
 
+
+
+
 const createMap = () => {
     for(let i = 0; i < 7; i++){
-        let createColum = document.createElement("section");
+        const createColum = document.createElement("section");
         for(let j = 0; j < 6; j++){
-            let createCell = document.createElement("div");
-            createColum.appendChild(createCell)
+         const createCell = document.createElement("div");
+            createColum.appendChild(createCell);
+            createColum.addEventListener('click', mark)
+           
         }
         document.querySelector("main").appendChild(createColum)
+        
     }
     
+ 
 }
 
-createMap()
-
 //---Andre
+
+const mark=(e)=>{
+
+const target = e.target;
+const change = e.target.children;
+for(let i=5; i >= 0; i--){
+    let n = change[i];
+//Verificando se o n possui estilo
+console.log(n.style.backgroundColor);
+    if(n.style.backgroundColor === ""){
+        if(player === 0 ){
+    
+            n.style.backgroundColor="red";
+            
+            player=1;
+            
+            }else{
+                n.style.backgroundColor="black";
+            
+                player=0;
+                 
+            }
+        break;
+    }else{
+        // n.style.backgroundColor="red";
+    
+        continue;
+    }
+       
+   
+}
+
+
+
+
+
+
+
+
+}
+createMap();
+
+
+
+
+
+
